7c7551ea47a960e7f5d90a7b1540b85b8da67ca0
Diego Quispe <72992112+diegoalonsoqj@users.noreply.github.com>
2023-07-14 17:57:25 -0500

    v2.0
    # Script para Monitoreo de Directorio y Modificación de Permisos
    
    Este script de shell utiliza la utilidad `inotifywait` para monitorear cambios en un directorio especificado y, al detectar la creación de un nuevo archivo o la llegada de un archivo movido, cambia sus permisos a 777.
    
    ## Funcionalidad Paso a Paso:
    
    1. El script comienza definiendo dos variables: `MONITOR_DIR`, que almacena la ruta del directorio a monitorear, y `LOG_FILE`, que almacena la ruta del archivo de registro (log) donde se registrarán todos los eventos.
    
    2. Luego, el script imprime su propio PID (Identificador de Proceso) en el archivo de registro.
    
    3. Se ejecuta el comando `inotifywait` en segundo plano (background) para monitorear continuamente el directorio especificado por la creación de nuevos archivos o la llegada de archivos movidos. El PID de este proceso `inotifywait` también se registra en el archivo de log.
    
    4. Los eventos de `inotifywait` se redirigen a un archivo temporal. Se usa un comando `tail -f` para leer este archivo continuamente.
    
    5. En un bucle, el script lee la salida de `inotifywait` (línea por línea) desde este archivo temporal. Cada línea contiene el directorio del evento, el tipo de evento y el nombre del archivo involucrado.
    
    6. El script luego registra el evento en el archivo de registro con la fecha y hora actuales, el nombre del archivo, el directorio y la acción que ocurrió.
    
    7. A continuación, se intenta cambiar los permisos del archivo a 777 utilizando `chmod`. Si el comando es exitoso, se registra un mensaje de éxito en el archivo de log. De lo contrario, se registra un mensaje de error.
    
    8. Finalmente, cuando se detiene el script, se configura una trampa para limpiar el archivo temporal y detener el proceso `inotifywait`.
    
    Este script es útil para tareas de administración de sistemas donde es necesario monitorear un directorio para nuevas llegadas de archivos y cambiar automáticamente sus permisos.

--- detectNewFiles.sh
+++ detectNewFiles.sh
@@ -3,14 +3,27 @@
 MONITOR_DIR="/Documentos"
 LOG_FILE="/var/log/scriptsDBA/detectNewFiles.log"
 
-inotifywait -m $MONITOR_DIR -e create -e moved_to |
-    while read dir action file; do
-        echo "[date '+%Y-%m-%d %H:%M:%S'] - El archivo '$file' aparecio en el directorio '$dir' por accion '$action'" >> $LOG_FILE
-        chmod 777 "$dir$file"
-        if [ $? -eq 0 ]
-        then
-            echo "[date '+%Y-%m-%d %H:%M:%S'] - El archivo '$file' ha sido modificado con exito a 777" >> $LOG_FILE
-        else
-            echo "[date '+%Y-%m-%d %H:%M:%S'] - La modificacion de permisos fallo para el archivo '$file'" >> $LOG_FILE
-        fi
-    done
+# Captura el PID del script
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] - El PID de este script es $$" >> $LOG_FILE
+
+# Se lanza inotifywait en background y se captura su PID
+inotifywait -m $MONITOR_DIR -e create -e moved_to --format '%w %e %f' --outfile /tmp/inotify-output.$$ &
+INOTIFY_PID=$!
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] - El PID de inotifywait es $INOTIFY_PID" >> $LOG_FILE
+
+# Procesa la salida de inotifywait
+tail -f /tmp/inotify-output.$$ | while read dir action file
+do
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] - El archivo '$file' aparecio en el directorio '$dir' por accion '$action'" >> $LOG_FILE
+    chmod 777 "${dir}${file}"
+    if [ $? -eq 0 ]
+    then
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] - El archivo '$file' ha sido modificado con exito a 777" >> $LOG_FILE
+    else
+        echo "[$(date '+%Y-%m-%d %H:%M:%S')] - La modificacion de permisos fallo para el archivo '$file'" >> $LOG_FILE
+    fi
+done
+
+# Limpia al salir
+trap "rm -f /tmp/inotify-output.$$; kill $INOTIFY_PID" EXIT
